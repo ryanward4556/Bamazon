58cd5251fd80631a83887181cf6b3f3797f21a3a
Ryan Ward <ryanward4556@gmail.com>
2019-08-10 17:26:13 -0400

    questions firing, need to update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,16 +2,13 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
 
-
 var connection = mysql.createConnection({
     host: "localhost",
-    // Your port; if not 3306
     port: 3306,
     user: "root",
     password: "@gxYSCx&Y%sf",
     database: "bamazon_db"
 });
-
 
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
@@ -21,49 +18,97 @@ function afterConnection() {
     });
 }
 
-inquirer
-    .prompt([
-        {
-            message: "What is the id of the product you want to buy?",
-            type: "number",
-            name: "id",
-        },
-        {
-            message: "How many units would you like to buy",
-            type: "number",
-            name: "unitsWanted",
-        }
-    ])
-    .then(answers => {
+const buyProduct = () => {
 
-        let id = answers.id;
-        let units = parseInt(answers.unitsWanted);
-        console.log(id);
-        console.log(units);
-        displayInfo();
-        // postItem(name, price, category);
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        if (err) throw err;
+        // console.log(res);
+        // console.log(res.affectedRows + ' product inserted!\n');
+        // Call updateProduct AFTER the INSERT completes
         // displayInfo();
+
+        inquirer.prompt([
+            {
+                name: "name",
+                type: "rawlist",
+                message: "What is the id of the product you want to buy?",
+                choices: function () {
+                    const choicesArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].product_name);
+                    }
+                    choicesArr.push(new inquirer.Separator());
+                    return choicesArr;
+                },
+            },
+            {
+                name: "units",
+                type: "number",
+                message: "How many units would you like to buy",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(answers => {
+            const name = answers.name;
+            const units = parseInt(answers.units);
+
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+              if (results[i].product_name === answers.name) {
+                chosenItem = results[i];
+              }
+            };
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: function (units) {
+                            const newStockQuantity = stock_quantity - units;
+                            return newStockQuantity;
+                        }
+                    },
+                    {
+                        product_name: chosenItem.id
+                    }
+                ],
+                function (error) {
+                    if (error) throw err;
+                    console.log("Bid placed successfully!");
+                    start();
+                }
+            );
+            afterConnection();
+        })
+        // console.log('Placing order for ' + units + 'units for ' + name + '...\n');
+
     });
 
-
-const postItem = (name, price, category) => {
-
-    console.log('Inserting a new product...\n');
-    var query = connection.query(
-        'INSERT INTO products SET ?',
-        {
-            name: name,
-            price: price,
-            category: category,
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + ' product inserted!\n');
-            // Call updateProduct AFTER the INSERT completes
-            displayInfo();
-        },
-    );
 };
+buyProduct();
+
+const testInq = () => {
+    inquirer.prompt([
+        {
+            name: "choice",
+            type: "rawlist",
+            choices: [1, 2, 3],
+            message: "choose a number"
+        },
+        {
+            name: "number",
+            type: "number",
+            message: "how many"
+        }
+    ]).then(answers => {
+        console.log(answers);
+    })
+}
+// testInq();
 
 const displayInfo = () => {
     connection.query("SELECT * FROM products", function (err, res) {
@@ -72,7 +117,7 @@ const displayInfo = () => {
 
         var table = new Table({
             head: ['id', 'product', 'department', 'price', 'in-stock'],
-            colWidths: [5, 50, 15, 12, 12],
+            colWidths: [5, 75, 15, 12, 12],
         });
 
         for (let i = 0; i < res.length; i++) {
@@ -83,5 +128,4 @@ const displayInfo = () => {
         console.log(table.toString());
 
     })
-    afterConnection();
 };
